38552616a312ac885212148a291dfa191b836b7a
Fabian Kaschta <f.kaschta@bsg-mm.de>
2025-08-29 15:59:54 +0200

    fixed missing exports in module-info

--- kafx/src/main/java/module-info.java
+++ kafx/src/main/java/module-info.java
@@ -1,4 +1,19 @@
 module org.openjfx.kafx {
-    requires javafx.controls;
-	requires java.logging;
+    requires transitive javafx.controls;
+	requires transitive java.logging;
+	exports org.openjfx.kafx.controller;
+	exports org.openjfx.kafx.io;
+	exports org.openjfx.kafx.lang;
+	exports org.openjfx.kafx.secure;
+	exports org.openjfx.kafx.view.alert;
+	exports org.openjfx.kafx.view.color;
+	exports org.openjfx.kafx.view.control;
+	exports org.openjfx.kafx.view.converter;
+	exports org.openjfx.kafx.view.dialog;
+	exports org.openjfx.kafx.view.dialog.userinput;
+	exports org.openjfx.kafx.view.listview;
+	exports org.openjfx.kafx.view.menu;
+	exports org.openjfx.kafx.view.pane;
+	exports org.openjfx.kafx.view.tableview;
+	exports org.openjfx.kafx.view.treeview;
 }
